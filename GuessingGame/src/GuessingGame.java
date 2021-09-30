fa930bd52bbf555c018320c60eea997728bd9166
Rizzuto <S02862429>
2021-09-30 10:15:39 -0600

    Guessing game

--- GuessingGame/src/GuessingGame.java
+++ GuessingGame/src/GuessingGame.java
@@ -1,14 +1,19 @@
+import java.util.Random;
 import java.util.Scanner;
-
 public class GuessingGame {
 	static Scanner input;
 	public static void main(String[] args) {
 		input= new Scanner(System.in);
 		int guess; 
+		Random randGen;
 		// TODO Auto-generated method stub
 		System.out.println("what is your guess?");
 		guess = input.nextInt();
-		
+		System.out.println("the guess is" + guess);
+		guess = input.nextInt();
+		randGen = new Random();
+		int answer;
+		answer = randGen.nextInt(20 - 10 + 1) + 10;
 		System.out.println("the guess is" + guess);
 	}
 
