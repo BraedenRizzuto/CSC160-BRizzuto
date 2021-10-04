e89c4db5d7967bb237e67d0924c65b6bc798e932
Rizzuto <S02862429>
2021-10-04 10:05:57 -0600

    CheckSheet#1

---
+++ CheckSheet#1/CheckSheetIf.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CheckSheetIf {
+	static Scanner input;
+	public static void main(String[] args) {
+		input=new Scanner(System.in);
+		int numOne;
+		int numTwo;
+		System.out.print("Enter First Number");
+		numOne=input.nextInt();
+		System.out.print("Enter Second Number");
+		numTwo=input.nextInt();
+		if (numOne<numTwo)
+		{
+			System.out.print("Highest Number Is"+numTwo);
+		}
+		else
+		{
+			System.out.print("Highest Number Is"+numOne);
+		}
+				int numThree;
+		System.out.print("Enter Third Number");
+		numThree=input.nextInt();
+		}
+	}
+
+}
--- GuessingGame/src/GuessingGame.java
+++ GuessingGame/src/GuessingGame.java
@@ -1,20 +1,35 @@
+
+//Braeden Rizzuto 
+//Guessing Game 1
+//Due 9/30/21/
+//Description:learn program. User guesses number 1-20 and program guesses it 
+
+
+
 import java.util.Random;
 import java.util.Scanner;
 public class GuessingGame {
 	static Scanner input;
 	public static void main(String[] args) {
 		input= new Scanner(System.in);
-		int guess; 
-		Random randGen;
+		int guess; // User's guess
+		Random randGen; //Random generator object
 		// TODO Auto-generated method stub
+		
 		System.out.println("what is your guess?");
-		guess = input.nextInt();
+			// getting users guess
+		guess = input.nextInt(); 
 		System.out.println("the guess is" + guess);
 		guess = input.nextInt();
+		
 		randGen = new Random();
-		int answer;
+		int answer; // random computer generated number
+		
+			// formula is (high minus low + 1) + low
 		answer = randGen.nextInt(20 - 10 + 1) + 10;
 		System.out.println("the guess is" + guess);
+		
 	}
 
 }
+//I did not really know what I was doing and was just copying it off the board.
